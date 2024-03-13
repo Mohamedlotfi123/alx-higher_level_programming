45fadc4662931eb928f470e2ee0ef76e087566d3
root <root@DESKTOP-8VTPROF>
2024-03-13 13:33:32 -0700

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    """
+    Function prints from 1 to 100, and if the number is multiple of
+    3 print fizz or multiple of 5 ptint buzz, and if multiple of both
+    print fizzbuzz.
+
+    """
+    for i in range(101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end = " ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
