24d2d8d9abdaf9e1b1db1cb40cc017a7b34aa78c
root <root@DESKTOP-8VTPROF>
2024-03-13 12:51:14 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,10 +9,7 @@ def uppercase(str):
         str: string to be printed in uppercase
     """
     for char in str:
-        # Check if the character is lower
-        #if ord(char) in range(97, 123):
-            # convert it to upper:
-            # "the difference between upper and lower in ASCII is 32"
-            #char = chr(ord(char) - 32)
-        #print("{}".format(char), end="")
+        if ord(char) in range(97, 123):
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print()
