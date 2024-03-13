71e2cae8f9b6be75cfb57afbe324c2493508c7a9
root <root@DESKTOP-8VTPROF>
2024-03-13 13:15:11 -0700

    addition function

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    """
+    Function that adds two integers.
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Ruturn:
+        sum of a and b "a + b"
+    """
+    return a + b
