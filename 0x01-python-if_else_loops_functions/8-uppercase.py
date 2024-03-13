181d539fae09e9cc1d8bf4a1108b1352f3251b30
root <root@DESKTOP-8VTPROF>
2024-03-13 13:03:09 -0700

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """
+    Function print string in uppercase.
+
+    Args:
+        str: string to be printed in upper.
+    """
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{:s}".format(char), end="")
+    print()
