96f1ba4579fbc35e133d3c7644e6217308730e47
root <root@DESKTOP-8VTPROF>
2024-03-13 13:23:50 -0700

    Computes the power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    """
+    Function computes a to the power of b.
+
+    Args:
+        a: the base
+        b: the power
+
+    Return:
+        a to the power of b "a ^ b"
+    """
+    return a ** b
