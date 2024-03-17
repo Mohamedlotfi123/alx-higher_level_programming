a778728d15435255d22bf12d511a84fd648d9bfa
root <root@DESKTOP-8VTPROF>
2024-03-18 00:24:06 +0200

    Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    function prints a matrix of integers.
+
+    Args:
+        matrix: matrix to print.
+    """
+    for row in matrix:
+        for element in row:
+            print("{}".format(element), end=" ")
+        print()
