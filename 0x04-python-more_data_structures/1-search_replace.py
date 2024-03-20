e7250246fc09444a4e581d4b9491a98f9849849f
root <root@DESKTOP-8VTPROF>
2024-03-21 01:11:14 +0200

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, repalce):
+    """
+    function that replace all occurrences of an element by
+    another in a new list.
+
+    Args:
+        my_list: initial list.
+        search: element to raplace in the list.
+        repalce: new element.
+
+    Return:
+        new list with the replacement.
+    """
+    nlist = []
+    for value in my_list:
+        if value == search:
+            nlist.append(replace)
+        else:
+            nlist.append(value)
+    return nlist
