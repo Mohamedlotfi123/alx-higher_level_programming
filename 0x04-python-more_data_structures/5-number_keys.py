005844370be07eab8b842808753c7aef2207b4ca
root <root@DESKTOP-8VTPROF>
2024-03-21 01:49:41 +0200

    Numbe of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """
+    function returns the number of keys in a dictionary.
+
+    Args:
+        a_dictionary: dictionaty.
+
+    Return:
+        Number of keys in a dictionary.
+    """
+    list_of_keys = list(a_dictionary)
+    return len(list_of_keys)
