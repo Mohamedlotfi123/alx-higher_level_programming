4e25a288aa69cbca27de4d2b62f224287797d079
root <root@DESKTOP-8VTPROF>
2024-03-21 01:34:05 +0200

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    """
+    function returns a set of common elements of two sets.
+
+    Args:
+        set_1: first set of element.
+        set_2: second set of element.
+
+    Return:
+        set of the common element of the two sets.
+    """
+    return set_1 & set_2
