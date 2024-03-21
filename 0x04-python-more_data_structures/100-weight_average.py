3bc8379b79c27fdfc79ffe20df41326f5d12a72b
root <root@DESKTOP-8VTPROF>
2024-03-21 04:48:25 +0200

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    """
+    function returns the weighted average.
+
+    Args:
+        my_list: list of tuples.
+
+    Return:
+        weighted average.
+    """
+    if len(my_list)  == 0:
+        return 0
+    t_of_weight = 0
+    s = 0
+    for t in my_list:
+        score, weight = t
+        s += score * weight
+        t_of_weight += weight
+        print(s, t_of_weight)
+    return s / t_of_weight
