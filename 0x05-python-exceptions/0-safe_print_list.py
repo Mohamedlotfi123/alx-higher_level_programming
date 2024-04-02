45115e96ef62225396132038150287b654ccf527
root <root@DESKTOP-8VTPROF>
2024-04-02 21:13:55 +0200

    safe list printing.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/bin/usr/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    """
+    function that prints x elements of a list.
+
+    Args:
+        my_list: list contain any type.
+        x: number of elements to print.
+
+    Return:
+        the real number of element printed.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            count += 1
+    except IndexError:
+        pass
+    print("")
+    return count
