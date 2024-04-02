c06ecfb97c8c9b9b8902073b42ec3fad55c65449
root <root@DESKTOP-8VTPROF>
2024-04-03 01:48:32 +0200

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square():
+    """
+    Class that defines a square.
+
+    Attributes:
+        size: size of the square.
+    """
+    def __init__(self, size):
+        """
+        initializes the square size.
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
