ac92605abf6f8a96ee125f1c8944372c0edc7a3e
root <root@DESKTOP-8VTPROF>
2024-04-16 03:38:55 +0200

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" inherits_from function """
+
+
+def inherits_from(obj, a_class):
+    """
+    function check if an object is instance of a class that inherited
+    from the specified class or not.
+
+    Args:
+        obj: the object to check.
+        a_class: specifice class.
+
+    Return:
+        True if obj is instance of inherited class of a_class,
+        or False otherwise.
+    """
+    if issubclass(obj, a_class):
+        return True
+    return False
