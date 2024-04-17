343abcd1ec8e1030970510bad56b15f97c4883b4
root <root@DESKTOP-8VTPROF>
2024-04-17 21:47:39 +0200

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,6 +15,6 @@ def inherits_from(obj, a_class):
         True if obj is instance of inherited class of a_class,
         or False otherwise.
     """
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class) and not type(obj) == a_class:
         return True
     return False
