591cdd35565fc90a34fe9bcc1b93d1dbca902e4b
root <root@DESKTOP-8VTPROF>
2024-04-24 21:08:39 +0200

    fix 3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,11 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 my_list = list()
-for item in load_from_json_file("add_item.json"):
-    my_list.append(item)
+try:
+    for item in load_from_json_file("add_item.json"):
+        my_list.append(item)
+except Exception:
+    pass
 for arg in sys.argv[1:]:
     my_list.append(arg)
 save_to_json_file(my_list, "add_item.json")
