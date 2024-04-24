bcd5c78e685363d168cee3d653c44aaa9e554f22
root <root@DESKTOP-8VTPROF>
2024-04-24 02:07:28 +0200

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" To JSON string """
+
+
+import json
+
+def to_json_string(my_object):
+    """
+    function that returns the JSON representation of an object.
+
+    Args:
+        my_object: object to serialize.
+
+    Return:
+        Serialized object.
+    """
+    return json.dumps(my_object)
