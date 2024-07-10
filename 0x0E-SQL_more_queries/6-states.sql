dae2131491a367584eb3251819e1728adb5b5607
root <root@DESKTOP-8VTPROF>
2024-07-11 02:13:12 +0300

    States table.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	name VARCHAR(256) NOT NULL
 	);
